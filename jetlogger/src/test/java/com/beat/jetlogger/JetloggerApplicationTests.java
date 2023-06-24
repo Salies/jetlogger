f590799fad39aad80d6a86c8ae127affa7e77adb
Salies <serezane@protonmail.com>
2023-06-24 13:12:38 -0300

    init

---
+++ jetlogger/src/test/java/com/beat/jetlogger/JetloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beat.jetlogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JetloggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

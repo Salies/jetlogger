015e122c25f7e576405c965bd7155ae9f76903c7
Salies <serezane@protonmail.com>
2023-06-26 17:11:19 -0300

    feat: game creation

--- jetlogger/src/main/java/com/beat/jetlogger/controller/GameController.java
+++ jetlogger/src/main/java/com/beat/jetlogger/controller/GameController.java
@@ -1,5 +1,6 @@
 package com.beat.jetlogger.controller;
 
+import com.beat.jetlogger.model.Game;
 import com.beat.jetlogger.model.GameList;
 import com.beat.jetlogger.repository.GameListRepository;
 import com.beat.jetlogger.repository.GameRepository;
@@ -7,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.UUID;
 
@@ -25,7 +28,7 @@ public class GameController {
         return "game";
     }*/
 
-    @GetMapping("/list/{id}/game/create")
+    @GetMapping("/list/{id}/create-game")
     public String getCreateGame(@PathVariable("id") String listId, Model model) {
         GameList gameList = gameListRepository.findById(UUID.fromString(listId)).orElseThrow();
         String listName = gameList.getName();
@@ -33,5 +36,17 @@ public class GameController {
         model.addAttribute("format", "Adicionar");
         model.addAttribute("listId", listId);
         return "create-game";
+    }
+
+    @PostMapping("/list/{id}/create-game")
+    public String postCreateGame(
+            @RequestParam("name") String name,
+            @RequestParam("platform") String platform,
+            @PathVariable("id") String listId
+    ) {
+        GameList list = gameListRepository.findById(UUID.fromString(listId)).orElseThrow();
+        Game game = new Game(list, name, platform);
+        gameRepository.save(game);
+        return "redirect:/list/" + listId;
     }
 }
